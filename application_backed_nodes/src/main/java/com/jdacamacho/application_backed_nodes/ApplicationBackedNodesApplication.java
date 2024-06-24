2f6987fc87dfb348f0d2ef72c00ecfeeb759126a
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-24 11:23:25 -0500

    firs commit

---
+++ application_backed_nodes/src/main/java/com/jdacamacho/application_backed_nodes/ApplicationBackedNodesApplication.java
@@ -0,0 +1,13 @@
+package com.jdacamacho.application_backed_nodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBackedNodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBackedNodesApplication.class, args);
+	}
+
+}
---
+++ application_backed_nodes/src/test/java/com/jdacamacho/application_backed_nodes/ApplicationBackedNodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdacamacho.application_backed_nodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationBackedNodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
