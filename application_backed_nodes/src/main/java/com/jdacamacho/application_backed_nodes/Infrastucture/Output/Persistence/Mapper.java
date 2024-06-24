59966bfbf7a220bd87d75d6083fd384993c34e9f
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-24 11:48:31 -0500

    add: dependecy for model mapper

---
+++ application_backed_nodes/src/main/java/com/jdacamacho/application_backed_nodes/Infrastucture/Output/Persistence/Mapper.java
@@ -0,0 +1,12 @@
+package com.jdacamacho.application_backed_nodes.Infrastucture.Output.Persistence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Mapper {
+    @Bean
+    public ModelMapper createMapper(){
+        return new ModelMapper();
+    }
+}
