d308e3c912aa1a8cc19822baeaac360194c46a23
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-26 18:44:55 -0500

    add: validation about size of coordinates

--- application_backed_nodes/src/main/java/com/jdacamacho/application_backed_nodes/Infrastucture/Input/ControllerManageNode/DTORequest/CoordinateDTORequest.java
+++ application_backed_nodes/src/main/java/com/jdacamacho/application_backed_nodes/Infrastucture/Input/ControllerManageNode/DTORequest/CoordinateDTORequest.java
@@ -1,5 +1,7 @@
 package com.jdacamacho.application_backed_nodes.Infrastucture.Input.ControllerManageNode.DTORequest;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,9 +11,13 @@ import lombok.Data;
 public class CoordinateDTORequest {
 
     @NotNull(message = "Value X cannot be null")
+    @DecimalMin(value = "-84.0", message = "Value X must be greater than or equal to -84.0")
+    @DecimalMax(value = "84.0", message = "Value X must be less than or equal to 84.0")
     private double valueX;
 
     @NotNull(message = "Value Y cannot be null")
+    @DecimalMin(value = "-179.0", message = "Value Y must be greater than or equal to -179.0")
+    @DecimalMax(value = "179.0", message = "Value Y must be less than or equal to 179.0")
     private double valueY;
     
     public CoordinateDTORequest(){
